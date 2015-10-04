4e4eda97e8c5d54c1a3a8810fa48c1eadd0ecca0
Dean Welch <deanwelch286@gmail.com>
2015-10-04 20:40:09 +0100

    major refactoring of problem 1

--- Project_Euler_001_Multiples_of_3_and_5/Project_Euler_001_Multiples_of_3_and_5.py
+++ Project_Euler_001_Multiples_of_3_and_5/Project_Euler_001_Multiples_of_3_and_5.py
@@ -1,42 +1,13 @@
-'''
-Created on 21 Nov 2014
-
-@author: dean
-'''
-
-listOfNumbers = []
-
-
-def multiple3(n):
-    i = 0
-    while i < n:
-        
-        if i not in listOfNumbers:
-            listOfNumbers.append(i)
-            print i
-        i += 3
-        
-        
-def multiple5(n):
-    j = 0
-    while j < 1000:
-        
-        if j not in listOfNumbers:
-            listOfNumbers.append(j)
-            print j
-        j += 5
-
-
-def add_list_numbers():
-    total = 0
-    for item in listOfNumbers:
-        total += item
-    return total
+from time import time
 
 
 def run(n):
-    multiple3(n)
-    multiple5(n)
-    print add_list_numbers()
-    
-run(1000)
+    total = 0
+    for x in range(n):
+        if x % 5 == 0 or x % 3 == 0:
+            total += x
+    return total
+
+start = time()
+print run(1000)
+print time() - start
