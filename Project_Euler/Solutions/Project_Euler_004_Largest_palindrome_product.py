41cb236253d914cb7732a15f9a378781fbad332e
Dean Welch <deanwelch286@gmail.com>
2015-10-10 03:52:40 +0100

    made Euler 4 more efficient

--- Project_Euler/Solutions/Project_Euler_004_Largest_palindrome_product.py
+++ Project_Euler/Solutions/Project_Euler_004_Largest_palindrome_product.py
@@ -2,10 +2,12 @@
 
 def largest_palindrome(n):
     largest = 0
-    for i in xrange(10**(n-1), 10**n):
-        for j in xrange(10**(n-1), 10**n):
+    for i in xrange(10**n, 10**(n-1), -1):
+        for j in xrange(10**n, 10**(n-1), -1):
             number_to_check = i*j
-            if number_to_check > largest and check_if_palindrome(number_to_check):
+            if number_to_check < largest:
+                break
+            if check_if_palindrome(number_to_check):
                 largest = number_to_check
     return largest
 
