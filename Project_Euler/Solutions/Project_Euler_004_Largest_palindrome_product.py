f20c135ca5d355b519a4c44c9fee1ae7c88be296
Dean Welch <deanwelch286@gmail.com>
2015-10-10 03:00:50 +0100

    redone euler 4

--- Project_Euler/Solutions/Project_Euler_004_Largest_palindrome_product.py
+++ Project_Euler/Solutions/Project_Euler_004_Largest_palindrome_product.py
@@ -1,37 +1,17 @@
-'''
-Created on 21 Nov 2014
-
-@author: dean
-'''
 
 
-def multiply_all_n_length_numbers(n):
-    all_multiplies = []
-    for i in xrange(10**n,10**(n+1)):
-        for j in xrange(10**n, 10**(n+1)):
-            all_multiplies.append(str(i*j))
-    return all_multiplies
-    # ===========================================================================
-    # for thing in all_multiplies:
-    #     print thing
-    # ===========================================================================
+def largest_palindrome(n):
+    largest = 0
+    for i in xrange(10**(n-1), 10**n):
+        for j in xrange(10**(n-1), 10**n):
+            number_to_check = i*j
+            if number_to_check > largest and check_if_palindrome(number_to_check):
+                largest = number_to_check
+    return largest
 
 
-def check_if_palindrome(list_of_numbers):
-    all_palindromes = []
-    for item in list_of_numbers:
-        if item[::-1] == item:
-            all_palindromes.append(item)
-    return all_palindromes
+def check_if_palindrome(to_check):
+    string_to_check = str(to_check)
+    if string_to_check[::-1] == string_to_check:
+        return True
 
-
-def sort_array(array_to_be_sorted):
-    array_to_be_sorted.sort(key=int)
-    return array_to_be_sorted
-        
-allMultiples = multiply_all_n_length_numbers(2)
-unsortedList = check_if_palindrome(allMultiples)
-sortedList = sort_array(unsortedList)
-
-for item in sortedList:
-    print item
