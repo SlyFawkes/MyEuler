322646eaf046302ac82d8b776f221c687d168109
Dean Welch <deanwelch286@gmail.com>
2015-10-11 21:52:34 +0100

    small refactor of euler 4 and 5

--- Project_Euler/Solutions/Project_Euler_004_Largest_palindrome_product.py
+++ Project_Euler/Solutions/Project_Euler_004_Largest_palindrome_product.py
@@ -3,7 +3,7 @@
 def largest_palindrome(n):
     largest = 0
     for i in xrange(10**n, 10**(n-1), -1):
-        for j in xrange(10**n, 10**(n-1), -1):
+        for j in xrange(i, 10**(n-1), -1):
             number_to_check = i*j
             if number_to_check < largest:
                 break
--- Project_Euler/Solutions/Project_Euler_005_Smallest_multiple.py
+++ Project_Euler/Solutions/Project_Euler_005_Smallest_multiple.py
@@ -1,14 +1,10 @@
-'''
-Created on 24 Nov 2014
 
-@author: dean
-'''
 primeFactors = []
 
 
 def det_all_primes(number, i):
     for item in primeFactors:
-        if number%item == 0:
+        if number % item == 0:
             number = number/item
     while i <= number:
         if number % i == 0:
@@ -18,7 +14,7 @@ def det_all_primes(number, i):
         i += 1
 
 
-def get_lcm(n):
+def smallest_multiple(n):
     for t in xrange(n+1):
         det_all_primes(t, 2)
     total_product = 1
@@ -26,4 +22,4 @@ def get_lcm(n):
         total_product *= item
     print total_product
 
-get_lcm(20)
+smallest_multiple(20)
