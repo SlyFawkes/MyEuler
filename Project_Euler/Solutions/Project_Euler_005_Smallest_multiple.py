b01a38ebf81c2782653185bb181d3d55b7e061e3
Dean Welch <deanwelch286@gmail.com>
2015-10-13 05:02:57 +0100

    minor cleanup of Euler 005

--- Project_Euler/Solutions/Project_Euler_005_Smallest_multiple.py
+++ Project_Euler/Solutions/Project_Euler_005_Smallest_multiple.py
@@ -1,16 +1,12 @@
 
-
-def get_unique_primes(number):
+def smallest_multiple(n):
     unique_prime_factors = []
-    for value in xrange(2, number+1):
+
+    for value in xrange(2, n+1):
         for prime in unique_prime_factors:
             if value % prime == 0:
                 value = value/prime
         unique_prime_factors.append(value)
-    return unique_prime_factors
 
-
-def smallest_multiple(n):
-    unique_prime_factors = get_unique_primes(n)
     total_product = reduce(lambda x, y: x*y, unique_prime_factors)
     return total_product
