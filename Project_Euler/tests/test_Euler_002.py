6cbc34cf1e8b48d349f4289e67282a5a92a302c7
Dean Welch <deanwelch286@gmail.com>
2015-10-14 23:41:30 +0100

    updated test for Euler 002 and fixed class name

--- Project_Euler/tests/test_Euler_002.py
+++ Project_Euler/tests/test_Euler_002.py
@@ -2,11 +2,13 @@ import unittest
 from Project_Euler.Solutions.Project_Euler_002_Even_Fibonacci_numbers import *
 
 
-class TestEuler001(unittest.TestCase):
+class TestEuler002(unittest.TestCase):
 
     def test_fibbonacci(self):
-        answer = fibbonacci(4000000)
-        self.assertEqual(answer, 4613732)
+        self.assertEqual(fibbonacci(4000000), 4613732)
+        self.assertEqual(fibbonacci(0), 0)
+        self.assertEqual(fibbonacci(1), 0)
+        self.assertEqual(fibbonacci(-1), 0)
 
 if __name__ == '__main__':
     unittest.main()
