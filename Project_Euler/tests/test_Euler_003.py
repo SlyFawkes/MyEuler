6a0dc19805fea9e46a4cffa322abfb0d9afa2782
Dean Welch <deanwelch286@gmail.com>
2015-10-15 19:49:46 -0400

    slight change to test Euler 003

--- Project_Euler/tests/test_Euler_003.py
+++ Project_Euler/tests/test_Euler_003.py
@@ -5,8 +5,7 @@ from Project_Euler.Solutions.Project_Euler_003_Largest_prime_factor import *
 class TestEuler003(unittest.TestCase):
 
     def test_largest_prime_factor(self):
-        answer = largest_prime_factor(600851475143)
-        self.assertEqual(answer, 6857)
+        self.assertEqual(largest_prime_factor(600851475143), 6857)
 
 if __name__ == '__main__':
     unittest.main()
