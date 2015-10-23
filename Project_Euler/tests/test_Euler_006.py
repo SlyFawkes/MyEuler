8e7fed980479c4c2e318923de47f5d7acc2067cb
Dean Welch <deanwelch286@gmail.com>
2015-10-23 21:51:10 +0100

    small changes to test Euler 006

--- Project_Euler/tests/test_Euler_006.py
+++ Project_Euler/tests/test_Euler_006.py
@@ -5,16 +5,13 @@ from Project_Euler.Solutions.Project_Euler_006_Sum_square_difference import *
 class TestEuler006(unittest.TestCase):
 
     def test_sum_square_difference(self):
-        answer = sum_square_difference(100)
-        self.assertEqual(answer, 25164150)
+        self.assertEqual(sum_square_difference(100), 25164150)
 
     def test_sum_of_squares(self):
-        answer = sum_of_squares(100)
-        self.assertEqual(answer, 338350)
+        self.assertEqual(sum_of_squares(100), 338350)
 
     def test_square_of_sum(self):
-        answer = square_of_sum(100)
-        self.assertEqual(answer, 25502500)
+        self.assertEqual(square_of_sum(100), 25502500)
 
 
 if __name__ == '__main__':
