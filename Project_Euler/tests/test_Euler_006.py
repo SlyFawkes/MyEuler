215d67dcdf87b15e4e090ccd6743ad3dba5f720a
Dean Welch <deanwelch286@gmail.com>
2015-10-24 23:39:47 -0700

    added more tests to test_Euler_006

--- Project_Euler/tests/test_Euler_006.py
+++ Project_Euler/tests/test_Euler_006.py
@@ -6,12 +6,18 @@ class TestEuler006(unittest.TestCase):
 
     def test_sum_square_difference(self):
         self.assertEqual(sum_square_difference(100), 25164150)
+        self.assertEqual(sum_square_difference(0), 0)
+        self.assertEqual(sum_square_difference(-1), 0)
 
     def test_sum_of_squares(self):
         self.assertEqual(sum_of_squares(100), 338350)
+        self.assertEqual(sum_of_squares(0), 0)
+        self.assertEqual(sum_of_squares(-1), 0)
 
     def test_square_of_sum(self):
         self.assertEqual(square_of_sum(100), 25502500)
+        self.assertEqual(square_of_sum(0), 0)
+        self.assertEqual(square_of_sum(-1), 0)
 
 
 if __name__ == '__main__':
