98ec39d7407c6d8223404396ebb64e38dfe7cdd1
Dean Welch <deanwelch286@gmail.com>
2015-10-25 23:11:17 +0000

    small refactor to test euler 007

--- Project_Euler/tests/test_Euler_007.py
+++ Project_Euler/tests/test_Euler_007.py
@@ -5,8 +5,7 @@ from Project_Euler.Solutions.Project_Euler_007_10001st_prime import *
 class TestEuler007(unittest.TestCase):
 
     def test_find_nth_prime(self):
-        answer = find_nth_prime(10001)
-        self.assertEqual(answer, 104743)
+        self.assertEqual(find_nth_prime(10001), 104743)
 
 
 if __name__ == '__main__':
