e08fedf3eb8e2795d4cc4a46eb1061ea0d9e8a11
Dean Welch <deanwelch286@gmail.com>
2015-11-01 22:06:49 -0700

    made Euler 009 pep 8 compliant

--- Project_Euler/Solutions/Project_Euler_009_Special_Pythagorean_triplet.py
+++ Project_Euler/Solutions/Project_Euler_009_Special_Pythagorean_triplet.py
@@ -1,32 +1,29 @@
-'''
-Created on 24 Nov 2014
 
-@author: dean
-'''
-
-def specialTriplet():
-    for n in xrange(1,500):
-        for m in xrange(1,500):
+def special_triplet():
+    for n in xrange(1, 500):
+        for m in xrange(1, 500):
             if m*(m+n) == 500:
                 return m, n
                 
 primeFactors = []
-def detAllPrimes(number, i):
+
+
+def det_all_primes(number, i):
     for item in primeFactors:
-        if number%item == 0:
+        if number % item == 0:
             number = number/item
     while i <= number:
         if number % i == 0:
             primeFactors.append(i)
-            detAllPrimes(number/i, i)
+            det_all_primes(number/i, i)
             break
-        i +=1
+        i += 1
 
-detAllPrimes(500, 2)
-numbers = specialTriplet()
+det_all_primes(500, 2)
+numbers = special_triplet()
 a = numbers[0]**2-numbers[1]**2
 b = 2*numbers[0]*numbers[1]
 c = numbers[1]**2 + numbers[0]**2
-print a,b,c
+print a, b, c
 
 print a*b*c
