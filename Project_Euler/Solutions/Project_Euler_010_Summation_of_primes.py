34ad70d23549df3279f3ea1fae9a78fe260d0152
Dean Welch <deanwelch286@gmail.com>
2015-11-04 23:57:59 +0000

    made Euler 010 pep 8 compliant

--- Project_Euler/Solutions/Project_Euler_010_Summation_of_primes.py
+++ Project_Euler/Solutions/Project_Euler_010_Summation_of_primes.py
@@ -1,42 +1,30 @@
-'''
-Created on 24 Nov 2014
 
-@author: dean
-'''
-
-import time
-
-#@profile
-def findAllPrimesBelow(k):
-    primes = [2, 3]
+def find_all_primes_below(k):
+    prime_list = [2, 3]
 
     t = 2
-    j = primes[-1] + 2
+    j = prime_list[-1] + 2
 
     while j < k:
         u=1
         while u < t:
-            p = primes[u]
+            p = prime_list[u]
             if j%p == 0:
                 j +=2
                 u = 1
             elif p**2 > j:
                 break
             else:
-                u+=1
+                u += 1
 
-        primes.append(j)
+        prime_list.append(j)
         j += 2
         t += 1
-    primes.pop()
-    return primes
+    prime_list.pop()
+    return prime_list
 
-
-start = time.time()
-primes = findAllPrimesBelow(1000)
+primes = find_all_primes_below(1000)
 total = 0
 for item in primes:
     total += item
 print total
-end = time.time()
-print end - start
