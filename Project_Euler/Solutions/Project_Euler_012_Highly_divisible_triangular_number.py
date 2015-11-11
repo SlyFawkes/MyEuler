c09e71428da345c3f7374f0a108ebdfbcd652149
Dean Welch <deanwelch286@gmail.com>
2015-11-11 23:51:50 +0000

    made Euler 12 pep 8 compliant

--- Project_Euler/Solutions/Project_Euler_012_Highly_divisible_triangular_number.py
+++ Project_Euler/Solutions/Project_Euler_012_Highly_divisible_triangular_number.py
@@ -1,33 +1,27 @@
-'''
-Created on 25 Nov 2014
-
-@author: dean
-'''
-import time
-#@profile
 import math
-def getTriNumber(n):
+
+
+def get_tri_number(n):
     i = 1
     total = 0
-    mostFactors = 1
-    while mostFactors <n:
-        total +=i
-        check = getNumOfFactors(total)
-        if check > mostFactors:
-            mostFactors = check
-            print mostFactors
+    most_factors = 1
+    while most_factors < n:
+        total += i
+        check = get_num_of_factors(total)
+        if check > most_factors:
+            most_factors = check
+            print most_factors
         i += 1
     return total
-         
-#@profile
-def getNumOfFactors(number):
+
+
+def get_num_of_factors(number):
     counter = 2
     y = int(math.sqrt(number))
-    for x in xrange(2,y):
-        if number%x ==0:
+    for x in xrange(2, y):
+        if number % x == 0:
             counter += 2
 
     return counter
-start = time.time()
-print getTriNumber(500)
-print time.time()-start
+
+print get_tri_number(500)
