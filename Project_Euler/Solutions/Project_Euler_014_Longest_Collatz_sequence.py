75f7519339af5e58773e7c8a761e4c8951004aca
Dean Welch <deanwelch286@gmail.com>
2015-11-13 14:54:35 +0000

    made Euler 14 pep 8 compliant

--- Project_Euler/Solutions/Project_Euler_014_Longest_Collatz_sequence.py
+++ Project_Euler/Solutions/Project_Euler_014_Longest_Collatz_sequence.py
@@ -1,29 +1,23 @@
-'''
-Created on 29 Nov 2014
 
-@author: Dean
-'''
-
-def collatz(maxNumber):
-    numberWithLongestChain = 0
-    maxCount = 0
-    for item in xrange(2,maxNumber+1):
-        currentNumber = item
+def collatz(max_number):
+    number_with_longest_chain = 0
+    max_count = 0
+    for item in xrange(2, max_number+1):
+        current_number = item
         count = 0
-        while currentNumber > 1:
-            if currentNumber%2 == 0:
-                currentNumber /= 2
+        while current_number > 1:
+            if current_number % 2 == 0:
+                current_number /= 2
             else:
-                currentNumber = (currentNumber*3)+1
+                current_number = (current_number*3)+1
             count += 1
 
-        if count > maxCount:
-            maxCount = count
-            numberWithLongestChain = item
-            print numberWithLongestChain
-            print maxCount
+        if count > max_count:
+            max_count = count
+            number_with_longest_chain = item
+            print number_with_longest_chain
+            print max_count
             print ""
-    print numberWithLongestChain
+    print number_with_longest_chain
 
 collatz(1000000)
-            
